efcf8513ca2ea9be40dcbf37ac6abb87e1f5820c
Marius Causemann <mariuscausemann@yahoo.de>
2020-09-17 13:34:31 +0200

    add env vars for offscreen ploting

--- saga/slurm-jobscript.sh
+++ saga/slurm-jobscript.sh
@@ -2,5 +2,8 @@
 # properties = {properties}
 module purge
 module load iimpi/2019b
-export  MUMPS OOC TMPDIR=/tmp/
+export MUMPS_OOC_TMPDIR=/tmp/
+export SINGULARITYENV_DISPLAY=:99.0
+export SINGULARITYENV_PYVISTA_OFF_SCREEN=true
+export SINGULARITYENV_YVISTA_USE_PANEL=false
 {exec_job}
